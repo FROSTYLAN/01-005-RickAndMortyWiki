30ef674439373c84ac9592582d91084ecbf3901e
FROSTYLAN <castillo089frosty@gmail.com>
2022-07-29 11:37:10 -0500

    loader and lazi img

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import LocationInfo from './components/LocationInfo';
-import SearchBox from './components/SearchBox';
-import ResindentsList from './components/ResindentsList';
-import titleIntro from './images/titleIntro.svg'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import LocationInfo from "./components/LocationInfo";
+import SearchBox from "./components/SearchBox";
+import ResindentsList from "./components/ResindentsList";
+import titleIntro from "./images/titleIntro.svg";
+import "./loader.css";
 
 function App() {
+  const [location, setLocation] = useState({});
+  const [loader, setLoader] = useState(true);
 
-  const [location, setLocation] = useState({})
-
-  useEffect(() =>{
+  useEffect(() => {
     const random = Math.floor(Math.random() * 126) + 1;
-    axios.get(`https://rickandmortyapi.com/api/location/${random}`)
-      .then(res => setLocation(res.data));
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${random}`)
+      .then((res) => {
+        setLocation(res.data);
+        setLoader(false);
+      });
   }, []);
 
   //console.log(location);
 
   return (
     <div className="App">
-      <div className='imgIntro'><img src={titleIntro} alt="" /></div>
+      <div className="imgIntro">
+        <img src={titleIntro} alt="" />
+      </div>
       <SearchBox setLocation={setLocation} />
-      <LocationInfo location={location}/>
-      <ResindentsList residents={location.residents} />
+      {loader ? (
+        <div class="lds-facebook">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      ) : (
+        <>
+          <LocationInfo location={location} />
+          <ResindentsList residents={location.residents} />
+        </>
+      )}
     </div>
   );
 }
--- src/components/ResidentInfo.js
+++ src/components/ResidentInfo.js
@@ -1,36 +1,42 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import LazyLoad from "react-lazy-load";
 
-const ResidentInfo = ({url}) => {
-    
-    const [resident, setResindent] = useState({})
+const ResidentInfo = ({ url }) => {
+  const [resident, setResindent] = useState({});
 
-    useEffect(() => {
-        axios.get(url)
-            .then(res => setResindent(res.data))
-    },[url])
+  useEffect(() => {
+    axios.get(url).then((res) => setResindent(res.data));
+  }, [url]);
 
-    //console.log(resident);
+  //console.log(resident);
 
-    return (
-        <div className='residentInfo'>
-            <div><img src={resident.image} alt="resident" /></div>
-            <div>
-                <h3> {resident.name} </h3>
-                <p> {resident.status} - {resident.species} </p>
-                <br />
-                <div>
-                    <small>origin:</small>
-                    <p>{resident.origin?.name}</p>
-                </div>
-                <br />
-                <div>
-                    <small>episodes where appear:</small>
-                    <p>{resident.episode?.length}</p>
-                </div>
-            </div>
+  return (
+    <div className="residentInfo">
+      <div>
+        <LazyLoad width={"100%"} debounce={false} offsetVertical={200}>
+          <img src={resident.image} alt="resident" />
+        </LazyLoad>
+      </div>
+      <div>
+        <h3> {resident.name} </h3>
+        <p>
+          {" "}
+          {resident.status} - {resident.species}{" "}
+        </p>
+        <br />
+        <div>
+          <small>origin:</small>
+          <p>{resident.origin?.name}</p>
         </div>
-    );
+        <br />
+        <div>
+          <small>episodes where appear:</small>
+          <p>{resident.episode?.length}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ResidentInfo;
